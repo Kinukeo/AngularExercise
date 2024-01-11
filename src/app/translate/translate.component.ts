ee8caa244305c4fbe67d36368f0312c7dc39121d
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-01-11 14:39:48 +0000

    feat: added cdkDrop and angular material packages

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { JapaneseWord } from '../japaneseWord';
 
 @Component({
   selector: 'app-translate',
   standalone: true,
-  imports: [],
+  imports: [CdkDrag, CdkDropList],
   templateUrl: './translate.component.html',
   styleUrl: './translate.component.css',
 })
@@ -13,10 +14,11 @@ export class TranslateComponent {
     {
       id: 1,
       word: 'わたしの',
+      englishWord: 'watashi no',
     },
-    { id: 2, word: 'コート' },
-    { id: 3, word: 'は' },
-    { id: 4, word: 'あかい' },
-    { id: 5, word: 'です' },
+    { id: 2, word: 'コート', englishWord: 'kooto' },
+    { id: 3, word: 'は', englishWord: 'wa' },
+    { id: 4, word: 'あかい', englishWord: 'akai' },
+    { id: 5, word: 'です', englishWord: 'desu' },
   ];
 }
